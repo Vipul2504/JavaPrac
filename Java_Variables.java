ca84adc00dd35d44b56486d6fa49aadc0a447caf
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2024-10-03 21:41:20 +0530

    Var

---
+++ Java_Variables.java
@@ -0,0 +1,22 @@
+public class Java_Variables {
+
+    int memberVaribale;
+    static int staticVaribale = 100;
+
+    public void dummyMethod() {
+        byte localvaribale = 100;
+        System.out.println(localvaribale);
+    }
+
+    public int dummyMethod2(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Java_Variables Obj1 = new Java_Variables();
+        Java_Variables Obj2 = new Java_Variables();
+
+        System.out.println();
+    }
+
+}
---
+++ Pattern.java
@@ -0,0 +1,10 @@
+
+public class Pattern {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 5; j > 5; j--) {
+                System.out.println("*");
+            }
+        }
+    }
+}
