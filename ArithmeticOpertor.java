2aa73fdd1f89dfedf208eda07931fb441f1d5d07
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2024-10-06 19:10:20 +0530

    Arithemtic

---
+++ ArithmeticOpertor.java
@@ -0,0 +1,19 @@
+public class ArithmeticOpertor {
+    public static void main(String[] args) {
+        int division = 5 / 2;
+        System.out.println(division);
+
+        int Addition = 5 + 2;
+        System.out.println(Addition);
+
+        int Subtraction = 5 - 2;
+        System.out.println(Subtraction);
+
+        int Multiplication = 5 * 2;
+        System.out.println(Multiplication);
+
+        int Modulus = 5 % 2;
+        System.out.println(Modulus);
+
+    }
+}
